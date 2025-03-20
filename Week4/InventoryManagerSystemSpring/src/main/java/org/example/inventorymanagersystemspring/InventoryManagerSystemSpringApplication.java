039d9d4b08d6f6862a5c5075081bc2dc56fe5e9b
Stefcho1227 <stefan.ivanov1227@gmail.com>
2025-03-20 19:17:15 +0200

    Started Spring project

---
+++ Week4/InventoryManagerSystemSpring/src/main/java/org/example/inventorymanagersystemspring/InventoryManagerSystemSpringApplication.java
@@ -0,0 +1,13 @@
+package org.example.inventorymanagersystemspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryManagerSystemSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InventoryManagerSystemSpringApplication.class, args);
+    }
+
+}
